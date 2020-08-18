0a14f9e1322844901cec2d8b902e901936841b83
Alexander Köplinger <alex.koeplinger@outlook.com>
2020-08-18 10:03:24 +0000

    WIP

--- eng/actions/backport/index.js
+++ eng/actions/backport/index.js
@@ -4,27 +4,26 @@ const github = require('@actions/github');
 try {
   const auth_token = core.getInput('auth_token');
   const octokit = github.getOctokit(auth_token)
-
-  // extract the target branch name from the trigger phrase containing these characters: a-z, A-Z, digits, forward slash, dot, hyphen, underscore
-  console.log(`Extracting target branch`);
-  const comment_body = github.event.comment.body;
-  var regex = /\/backport to ([a-zA-Z\d\/\.\-\_]+)/;
-  const target_branch = regex.exec(comment_body)[1];
-  if (target_branch == null) throw "No backport branch found."
-  console.log(`Backport target branch: ` + target_branch);
-
-  // Post backport started comment to pull request
   const run_id = github.run_id;
   const repo_owner = github.repository_owner;
   const repo_name = github.event.repository.name;
   const pr_number = github.event.issue.number;
-  var comment_body = `Started backporting to ${target_branch}: https://github.com/${repo_owner}/${repo_name}/actions/runs/${run_id}`;
+
+  // extract the target branch name from the trigger phrase containing these characters: a-z, A-Z, digits, forward slash, dot, hyphen, underscore
+  console.log(`Extracting target branch`);
+  var regex = /\/backport to ([a-zA-Z\d\/\.\-\_]+)/;
+  const target_branch = regex.exec(github.event.comment.body)[1];
+  if (target_branch == null) throw "No backport branch found."
+  console.log(`Backport target branch: ` + target_branch);
+
+  // Post backport started comment to pull request
+  const backport_start_body = `Started backporting to ${target_branch}: https://github.com/${repo_owner}/${repo_name}/actions/runs/${run_id}`;
 
   octokit.issues.createComment({
     repo_owner,
     repo_name,
     pr_number,
-    comment_body
+    backport_start_body
   });
 } catch (error) {
   core.setFailed(error.message);
