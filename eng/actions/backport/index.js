3fea95b270228657c98ff410f44ed989a7576054
Alexander Köplinger <alex.koeplinger@outlook.com>
2020-08-18 11:26:00 +0000

    WIP

--- eng/actions/backport/index.js
+++ eng/actions/backport/index.js
@@ -8,30 +8,36 @@ console.log(`Finished installing npm dependencies`);
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const auth_token = core.getInput('auth_token');
-  const octokit = github.getOctokit(auth_token)
-  const run_id = github.run_id;
-  const repo_owner = github.repository_owner;
-  const repo_name = github.event.repository.name;
-  const pr_number = github.event.issue.number;
+if (github.context.eventName !== 'issue_comment') throw "This action only works on issue_comment events.";
 
-  // extract the target branch name from the trigger phrase containing these characters: a-z, A-Z, digits, forward slash, dot, hyphen, underscore
-  console.log(`Extracting target branch`);
-  var regex = /\/backport to ([a-zA-Z\d\/\.\-\_]+)/;
-  const target_branch = regex.exec(github.event.comment.body)[1];
-  if (target_branch == null) throw "No backport branch found."
-  console.log(`Backport target branch: ` + target_branch);
+async function run() {
+  try {
+    const auth_token = core.getInput('auth_token');
+    const octokit = github.getOctokit(auth_token)
+    const run_id = github.run_id;
+    const repo_owner = github.context.repo.owner;
+    const repo_name = github.context.repo.name;
+    const pr_number = github.context.issue.number;
 
-  // Post backport started comment to pull request
-  const backport_start_body = `Started backporting to ${target_branch}: https://github.com/${repo_owner}/${repo_name}/actions/runs/${run_id}`;
+    // extract the target branch name from the trigger phrase containing these characters: a-z, A-Z, digits, forward slash, dot, hyphen, underscore
+    console.log(`Extracting target branch`);
+    var regex = /\/backport to ([a-zA-Z\d\/\.\-\_]+)/;
+    const target_branch = regex.exec(github.context.comment.body)[1];
+    if (target_branch == null) throw "No backport branch found.";
+    console.log(`Backport target branch: ` + target_branch);
 
-  octokit.issues.createComment({
-    repo_owner,
-    repo_name,
-    pr_number,
-    backport_start_body
-  });
-} catch (error) {
-  core.setFailed(error.message);
+    // Post backport started comment to pull request
+    const backport_start_body = `Started backporting to ${target_branch}: https://github.com/${repo_owner}/${repo_name}/actions/runs/${run_id}`;
+
+    await octokit.issues.createComment({
+      repo_owner,
+      repo_name,
+      pr_number,
+      backport_start_body
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
