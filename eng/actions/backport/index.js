351c405ae97975c2d74f8b92d347837e675100d0
Alexander Köplinger <alex.koeplinger@outlook.com>
2020-08-18 11:03:04 +0000

    WIP

--- eng/actions/backport/index.js
+++ eng/actions/backport/index.js
@@ -1,43 +1,37 @@
-async function run() {
-  const util = require('util');
-  const exec = util.promisify(require('child_process').exec);
+const execSync = require('child_process').execSync;
 
-  console.log(`Installing npm dependencies`);
-  const { stdout, stderr } = await exec("sudo npm install -g @actions/core @actions/github");
-  console.log(`npm-install stderr: ${stderr}`);
-  console.log(`npm-install stdout: ${stdout}`);
-  console.log(`Finished installing npm dependencies`);
+console.log(`Installing npm dependencies`);
+const npm_stdout = execSync("sudo npm install -g @actions/core @actions/github");
+console.log(`npm-install stdout: ${npm_stdout}`);
+console.log(`Finished installing npm dependencies`);
 
-  const core = require('@actions/core');
-  const github = require('@actions/github');
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-  try {
-    const auth_token = core.getInput('auth_token');
-    const octokit = github.getOctokit(auth_token)
-    const run_id = github.run_id;
-    const repo_owner = github.repository_owner;
-    const repo_name = github.event.repository.name;
-    const pr_number = github.event.issue.number;
+try {
+  const auth_token = core.getInput('auth_token');
+  const octokit = github.getOctokit(auth_token)
+  const run_id = github.run_id;
+  const repo_owner = github.repository_owner;
+  const repo_name = github.event.repository.name;
+  const pr_number = github.event.issue.number;
 
-    // extract the target branch name from the trigger phrase containing these characters: a-z, A-Z, digits, forward slash, dot, hyphen, underscore
-    console.log(`Extracting target branch`);
-    var regex = /\/backport to ([a-zA-Z\d\/\.\-\_]+)/;
-    const target_branch = regex.exec(github.event.comment.body)[1];
-    if (target_branch == null) throw "No backport branch found."
-    console.log(`Backport target branch: ` + target_branch);
+  // extract the target branch name from the trigger phrase containing these characters: a-z, A-Z, digits, forward slash, dot, hyphen, underscore
+  console.log(`Extracting target branch`);
+  var regex = /\/backport to ([a-zA-Z\d\/\.\-\_]+)/;
+  const target_branch = regex.exec(github.event.comment.body)[1];
+  if (target_branch == null) throw "No backport branch found."
+  console.log(`Backport target branch: ` + target_branch);
 
-    // Post backport started comment to pull request
-    const backport_start_body = `Started backporting to ${target_branch}: https://github.com/${repo_owner}/${repo_name}/actions/runs/${run_id}`;
+  // Post backport started comment to pull request
+  const backport_start_body = `Started backporting to ${target_branch}: https://github.com/${repo_owner}/${repo_name}/actions/runs/${run_id}`;
 
-    octokit.issues.createComment({
-      repo_owner,
-      repo_name,
-      pr_number,
-      backport_start_body
-    });
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  octokit.issues.createComment({
+    repo_owner,
+    repo_name,
+    pr_number,
+    backport_start_body
+  });
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-run();
