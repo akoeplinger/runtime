aa61345c432a8fac294391af7ca2720ffd518ae1
Alexander Köplinger <alex.koeplinger@outlook.com>
2020-08-18 17:07:31 +0200

    Update EventSourceTestParser.cs

--- src/libraries/System.Diagnostics.Tracing/tests/BasicEventSourceTest/EventSourceTestParser.cs
+++ src/libraries/System.Diagnostics.Tracing/tests/BasicEventSourceTest/EventSourceTestParser.cs
@@ -328,7 +328,7 @@ using Address = System.UInt64;
 //                source.UnregisterEventTemplate(value, 30, ProviderGuid);
 //            }
 //        }
-//        public event Action<EmptyTraceData> LogTransferNoOpcode
+//        public event Action<EmptyTraceData> LogTransferNoOpcode CONFLICT
 //        {
 //            add
 //            {
