bc0f98ae4d70c110b6475bc652bf3f32eeef58f8
Alexander Köplinger <alex.koeplinger@outlook.com>
2020-08-18 11:29:53 +0000

    WIP

--- eng/actions/backport/index.js
+++ eng/actions/backport/index.js
@@ -1,16 +1,18 @@
-const execSync = require('child_process').execSync;
-
-console.log(`Installing npm dependencies`);
-const npm_stdout = execSync("npm install @actions/core @actions/github");
-console.log(`npm-install stdout: ${npm_stdout}`);
-console.log(`Finished installing npm dependencies`);
-
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-if (github.context.eventName !== 'issue_comment') throw "This action only works on issue_comment events.";
-
 async function run() {
+  const util = require('util');
+  const exec = util.promisify(require('child_process').exec);
+
+  console.log(`Installing npm dependencies`);
+  const { stdout, stderr } = await exec("npm install @actions/core @actions/github");
+  console.log(`npm-install stderr: ${stderr}`);
+  console.log(`npm-install stdout: ${stdout}`);
+  console.log(`Finished installing npm dependencies`);
+
+  const core = require('@actions/core');
+  const github = require('@actions/github');
+
+  if (github.context.eventName !== 'issue_comment') throw "This action only works on issue_comment events.";
+
   try {
     const auth_token = core.getInput('auth_token');
     const octokit = github.getOctokit(auth_token)
@@ -22,7 +24,7 @@ async function run() {
     // extract the target branch name from the trigger phrase containing these characters: a-z, A-Z, digits, forward slash, dot, hyphen, underscore
     console.log(`Extracting target branch`);
     var regex = /\/backport to ([a-zA-Z\d\/\.\-\_]+)/;
-    const target_branch = regex.exec(github.context.comment.body)[1];
+    const target_branch = regex.exec(github.context.payload.comment.body)[1];
     if (target_branch == null) throw "No backport branch found.";
     console.log(`Backport target branch: ` + target_branch);
 
